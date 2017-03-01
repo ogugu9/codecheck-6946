62036af3b7e0740cbde1398d93fd464df91405e9
Ogura Riku <e.cos.isin@gmail.com>
2017-03-01 20:34:17 +0900

    first answer

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 "use strict";
-
 function main(argv) {
-  argv.forEach(v => console.log(v));
+  let name = '';
+  if (argv.length > 0) {
+    name = argv[0]
+  }
+  console.log(`Hello ${name}!`);
 }
-
 module.exports = main;
